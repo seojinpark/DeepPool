25112ec0569cc4d0e60a70e6687f69966cd7cf7f
Sunghyun Kim <koishkim@gmail.com>
2021-07-29 16:12:49 -0400

    NCCL test code involving CUDA tensors and using their data pointers directly in NCCL primitives

--- csrc/comm_p2p_master.cpp
+++ csrc/comm_p2p_master.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <torch/torch.h>
 #include "cuda_runtime.h"
 #include "nccl.h"
 #include "communication.h"
+#include <iostream>
 
 #define CUDACHECK(cmd) do {                         \
   cudaError_t e = cmd;                              \
@@ -31,7 +33,7 @@ int comm_p2p_master(void)
 
   //managing 1 device(s) per node & 2 rank(s) in total
   int nDev = 1;
-  int size = 32*1024*1024;
+  int size = 9; // 32*1024*1024;
   int devs[1] = { 0 };
   int nRanks = 2;
   int myRank = 0; // hard-coded rank 0 for master
@@ -46,7 +48,7 @@ int comm_p2p_master(void)
     CUDACHECK(cudaSetDevice(i));
     CUDACHECK(cudaMalloc(sendbuff + i, size * sizeof(float)));
     CUDACHECK(cudaMalloc(recvbuff + i, size * sizeof(float)));
-    CUDACHECK(cudaMemset(sendbuff[i], 1, size * sizeof(float)));
+    CUDACHECK(cudaMemset(sendbuff[i], 0x44, size * sizeof(float)));
     CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
     CUDACHECK(cudaStreamCreate(s+i));
   }
@@ -70,19 +72,60 @@ int comm_p2p_master(void)
   NCCLCHECK(ncclCommInitRank(comms, nRanks, cliqueId, myRank));
   printf("ncclCommInitRank done!\n");
 
+  torch::Tensor send_tensor = torch::ones({3,3}, torch::Device(torch::kCUDA, 0));
+  torch::Tensor recv_tensor = torch::zeros({9}, torch::Device(torch::kCUDA, 0));
+
+  CUDACHECK(cudaMemcpy(send_tensor.data_ptr(), sendbuff[0], size*sizeof(float), cudaMemcpyDeviceToDevice));
+  CUDACHECK(cudaMemcpy(recv_tensor.data_ptr(), recvbuff[0], size*sizeof(float), cudaMemcpyDeviceToDevice));
+
+  std::cout << send_tensor << std::endl;
+  std::cout << send_tensor.data_ptr() << std::endl;
+
+  std::cout << recv_tensor << std::endl;
+  std::cout << recv_tensor.data_ptr() << std::endl;
+
+#if 0
+  float *h_sendbuff = (float *)malloc(size * sizeof(float));
+  float *h_recvbuff = (float *)malloc(size * sizeof(float));
+
+  CUDACHECK(cudaMemcpy(h_sendbuff, sendbuff[0], size*sizeof(float), cudaMemcpyDeviceToHost));
+  CUDACHECK(cudaMemcpy(h_recvbuff, recvbuff[0], size*sizeof(float), cudaMemcpyDeviceToHost));
+
+  printf("CUDA send tensor before comm:\n");
+  for (int i = 0; i < size; i++) printf("%f\n", h_sendbuff[i]);
+  printf("CUDA recv tensor before comm:\n");
+  for (int i = 0; i < size; i++) printf("%f\n", h_recvbuff[i]);
+#endif
+
    //calling NCCL communication API. Group API is required when using
    //multiple devices per thread
   NCCLCHECK(ncclGroupStart());
   for (int i = 0; i < nDev; ++i) {
     printf("rank %d sending to rank %d\n", myRank, (myRank+1)%2);
-    NCCLCHECK(ncclSend((void*)sendbuff[i], size, ncclFloat, (myRank+1)%2, comms[i], s[i]));
+    NCCLCHECK(ncclSend((void*)send_tensor.data_ptr(), size, ncclFloat, (myRank+1)%2, comms[i], s[i]));
     printf("rank %d recving from rank %d\n", myRank, (myRank+1)%2);
-    NCCLCHECK(ncclRecv((void*)recvbuff[i], size, ncclFloat, (myRank+1)%2, comms[i], s[i]));
+    NCCLCHECK(ncclRecv((void*)recv_tensor.data_ptr(), size, ncclFloat, (myRank+1)%2, comms[i], s[i]));
     /*NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclFloat, ncclSum,
         comms[i], s[i]));
     */
   }
   NCCLCHECK(ncclGroupEnd());
+
+#if 0
+  CUDACHECK(cudaMemcpy(h_sendbuff, sendbuff[0], size*sizeof(float), cudaMemcpyDeviceToHost));
+  CUDACHECK(cudaMemcpy(h_recvbuff, recvbuff[0], size*sizeof(float), cudaMemcpyDeviceToHost));
+
+  printf("CUDA sendbuff after comm:\n");
+  for (int i = 0; i < size; i++) printf("%f\n", h_sendbuff[i]);
+  printf("CUDA recvbuff after comm:\n");
+  for (int i = 0; i < size; i++) printf("%f\n", h_recvbuff[i]);
+#endif
+
+  std::cout << send_tensor << std::endl;
+  std::cout << send_tensor.data_ptr() << std::endl;
+
+  std::cout << recv_tensor << std::endl;
+  std::cout << recv_tensor.data_ptr() << std::endl;
 
   //synchronizing on CUDA streams to wait for completion of NCCL operation
   for (int i = 0; i < nDev; ++i) {
