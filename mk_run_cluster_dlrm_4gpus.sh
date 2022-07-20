fe62a2565a5993f7a6c8c53dcec566bd149bd83f
Mark Keaton <mark.keaton@raytheon.com>
2022-07-20 14:06:46 -0400

    Fix port numbers in cluster scripts

--- mk_run_cluster_dlrm_4gpus.sh
+++ mk_run_cluster_dlrm_4gpus.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH='/home/mkeaton/msccl/build/lib:/home/mkeaton/cudnn/cuda/l
 
 export SCCL_XML_FILES='/home/mkeaton/DeepPool/ar-n4-c4-i4.xml'
 
-NCCL_ALGO=SCCL python3 cluster.py --addrToBind localhost:15507 --c10dBackend nccl --be_batch_size=0 --cpp --profile --logdir /home/mkeaton/DeepPool/logs --gpus 4 --sync_coalesce --sync_tensor_pad 16
+NCCL_ALGO=SCCL python3 cluster.py --addrToBind localhost:12347 --c10dBackend nccl --be_batch_size=0 --cpp --profile --logdir /home/mkeaton/DeepPool/logs --gpus 4 --sync_coalesce --sync_tensor_pad 16
